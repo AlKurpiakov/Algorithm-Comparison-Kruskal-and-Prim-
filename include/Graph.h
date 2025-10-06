b13fb72b4cbe007542a312bbd64e6a2a254fcb4b
Aleksei Kurpiakov <166383450+AlKurpiakov@users.noreply.github.com>
2025-10-06 18:08:15 +0300

    Add files via upload

--- include/Graph.h
+++ include/Graph.h
@@ -1,12 +1,14 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <cstdint>
 #pragma once
 #include <limits.h>
 #include <cstddef>
 #include <iostream>
 #include <utility>
 #include <vector>
+#include <unordered_set>
 
 using weight = long long;
 const long long INF = 100000000;
@@ -15,28 +17,32 @@ const long long INF = 100000000;
 struct Edge{
     int _v, _u;     //стартовая вершина -> конечная вершина
     weight _weight; //вес ребра
-    bool operator<(Edge other);
+    Edge(int v, int u, weight w);
+    const bool operator<(Edge other) const;
+    const bool operator==(Edge other) const;
+    const uint64_t GetEdgeKey() const; 
 };
 
 class Graph{
 private:
 
     
-    int _num_of_vertex;                                            //количество вершин
+    int _num_of_vertex;                                               //количество вершин
     std::vector<Edge> _edges;                                         //ребра графа
     std::vector<std::vector<std::pair<int, weight>>> _adjacency_list; //список смежности
-    
+    std::unordered_set<uint64_t> _existing_edges;                     //вспомогательная структура для добавления ребер и генерации графов
     
 public:
     
     Graph(int n = 0);   
     const std::vector<std::pair<int, weight>> GetVertexList(int v) const;
     const int Size() const;
+    const int EdgeCount() const;
     const std::vector<Edge> GetEdges() const;
     void Resize(int n);
     void AddEdge(int v, int u, weight w);
     const bool IsConnected(int x, int y) const;
-    
+    const std::unordered_set<uint64_t>& GetExistingEdgesMap() const;
     friend std::istream& operator>>(std::istream& in, Graph& graph);  
 };
 
